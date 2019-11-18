9d2d55bd23de22abaab65516ef7fb911728dfe40
Johan Kotlinski <kotlinski@gmail.com>
2019-11-18 23:03:28 +0100

    more work

--- xc.py
+++ xc.py
@@ -6,15 +6,41 @@ def export(xt_words_, heap_, start_word):
 	xt_words = xt_words_
 	heap = heap_
 
-	export_word(start_word)
+	words_to_export.append(start_word)
+
+	while words_to_export:
+		word = words_to_export.pop()
+		export_word(word)
+
+exported_words = []
+words_to_export = []
 
 def export_word(w):
-	print(w.xt)
+	if w in exported_words:
+		return
+	exported_words.append(w)
+
+	xt = w.xt
+
+	if w.body:
+		export_forth_word(w)
+	else:
+		export_python_word(w)
+
+def export_forth_word(w):
+	print("export_forth_word", w)
 	for ip in range(w.body, w.body_end):
 		cell = heap[ip]
 		if callable(cell):
+			cell_word = xt_words[cell]
+			words_to_export.append(cell_word)
 			print(xt_words[cell])
 		elif type(cell) == type(0):
 			print(cell)
 		else:
 			sys.exit("Unknown cell type", cell)
+
+def export_python_word(w):
+	print("export_python_word", w)
+	pass
+	
