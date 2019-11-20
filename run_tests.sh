98c9d0367cfa4052c4edbb16debb017c3dc9f45e
Johan Kotlinski <kotlinski@gmail.com>
2019-11-20 22:30:50 +0100

    testing

--- run_tests.sh
+++ run_tests.sh
@@ -2,4 +2,4 @@ set -e
 ./htfc.py test/target-tester.fs test/target.fs
 acme -o target-test.prg -f cbm target-test.asm
 x64 target-test.prg
-echo "Accept text" | ./htfc.py test/tester.fs test/testutilities.fs test/testcore.fs test/testerrorreport.fs test/testcoreplus.fs test/testcoreext.fs
+echo "Accept text" | ./htfc.py test/tester.fs test/testutilities.fs test/testcore.fs test/testerrorreport.fs test/testcoreplus.fs test/testcoreext.fs test/report-errors.fs
