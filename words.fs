cb7d04dd1b43ae87a0fafa0846391adf5963f96b
Johan Kotlinski <kotlinski@gmail.com>
2019-11-20 10:23:03 +0100

    move primitives to words.fs

--- words.fs
+++ words.fs
@@ -193,3 +193,148 @@ swap 0 <# #s #> rot over - spaces type space ;
 	ldx	w	; restore x
 	jmp	(w2)
 ;code
+
+:code 0branch
+	inx
+	lda LSB-1, x
+	ora MSB-1, x
+	beq %branch%
+
+	; skip offset
+	pla
+	clc
+	adc #2
+	bcc +
+	tay
+	pla
+	adc #0
+	pha
+	tya
++	pha
+	rts
+;code
+
+:code !
+	lda LSB, x
+	sta W
+	lda MSB, x
+	sta W + 1
+
+	ldy #0
+	lda LSB+1, x
+	sta (W), y
+	iny
+	lda MSB+1, x
+	sta (W), y
+
+	inx
+	inx
+	rts
+;code
+
+:code negate
+	jsr %invert%
+	jmp %1+%
+;code
+
+:code 0<
+	lda	MSB,x
+	and	#$80
+	beq	+
+	lda	#$ff
++	sta	LSB,x
+	sta	MSB,x
+	rts
+;code
+
+:code dup
+	dex
+	lda MSB + 1, x
+	sta MSB, x
+	lda LSB + 1, x
+	sta LSB, x
+	rts
+;code
+
+:code type
+-	lda LSB,x
+	ora MSB,x
+	bne +
+	inx
+	inx
+	rts
++	jsr %over%
+	jsr %c@%
+	jsr %emit%
+	jsr %1%
+	jsr %/string%
+	jmp -
+;code
+
+:code depth
+	txa
+	eor #$ff
+	tay
+	iny
+	dex
+	sty LSB,x
+	lda #0
+	sta MSB,x
+	rts
+;code
+
+:code @
+	lda LSB,x
+	sta W
+	lda MSB,x
+	sta W+1
+
+	ldy #0
+	lda (W),y
+	sta LSB,x
+	iny
+	lda (W),y
+	sta MSB,x
+	rts
+;code
+
+:code =
+	ldy #0
+	lda LSB, x
+	cmp LSB + 1, x
+	bne +
+	lda MSB, x
+	cmp MSB + 1, x
+	bne +
+	dey
++	inx
+	sty MSB, x
+	sty LSB, x
+	rts
+;code
+
+:code (do)
+	pla
+	sta	W
+	pla
+	tay
+
+	lda	MSB+1,x
+	pha
+	lda	LSB+1,x
+	pha
+
+	lda	MSB,x
+	pha
+	lda	LSB,x
+	pha
+
+	inx
+	inx
+
+	tya
+	pha
+	lda	W
+	pha
+	rts
+;code
