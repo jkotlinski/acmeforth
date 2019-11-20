34a8d6dd5dd4c4d8e4d81d384f112042cdb39fb2
Johan Kotlinski <kotlinski@gmail.com>
2019-11-20 22:26:01 +0100

    simplify test runner

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,5 @@
-./htfc.py test/target-tester.fs test/target.fs && \
-acme -o target-test.prg -f cbm target-test.asm && \
-x64 target-test.prg && \
+set -e
+./htfc.py test/target-tester.fs test/target.fs
+acme -o target-test.prg -f cbm target-test.asm
+x64 target-test.prg
 echo "Accept text" | ./htfc.py test/tester.fs test/testutilities.fs test/testcore.fs test/testerrorreport.fs test/testcoreplus.fs test/testcoreext.fs
