23b57229d078ab37ab3c627f4f9b70782d3aa558
Johan Kotlinski <kotlinski@gmail.com>
2019-11-24 22:46:13 +0100

    add examples to tests

--- run_tests.sh
+++ run_tests.sh
@@ -3,3 +3,12 @@ set -e
 acme -o target-test.prg -f cbm target-test.asm
 x64 target-test.prg
 echo "Accept text" | ./htfc.py test/tester.fs test/testutilities.fs test/testcore.fs test/testerrorreport.fs test/testcoreplus.fs test/testcoreext.fs test/report-errors.fs
+
+# examples
+./htfc.py examples/sierp.fs
+acme -o sierp.prg -f cbm sierp.asm
+x64 sierp.prg
+
+./htfc.py examples/colorcycle.fs
+acme -o colorcycle.prg -f cbm colorcycle.asm
+x64 colorcycle.prg
