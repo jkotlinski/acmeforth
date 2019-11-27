1733ca9587a47bdb5d5c8809c0ae3f79782d47a0
Johan Kotlinski <kotlinski@gmail.com>
2019-11-27 17:15:34 +0100

    added balloon example

---
+++ examples/balloon.fs
@@ -0,0 +1,24 @@
+\ Up, up and away!
+\ Adapted from Commodore 64 Users Guide.
+
+create sprite
+0 c, 127 c, 0 c, 1 c, 255 c, 192 c, 3 c, 255 c, 224 c, 3 c, 231 c, 224 c,
+7 c, 217 c, 240 c, 7 c, 223 c, 240 c, 7 c, 217 c, 240 c, 3 c, 231 c, 224 c,
+3 c, 255 c, 224 c, 3 c, 255 c, 224 c, 2 c, 255 c, 160 c, 1 c, 127 c, 64 c,
+1 c, 62 c, 64 c, 0 c, 156 c, 128 c, 0 c, 156 c, 128 c, 0 c, 73 c, 0 c, 0 c, 73 c, 0 c,
+0 c, 62 c, 0 c, 0 c, 62 c, 0 c, 0 c, 62 c, 0 c, 0 c, 28 c, 0 c,
+
+: vsync
+begin $d011 c@ $80 and 0= until
+begin $d011 c@ $80 and until ;
+
+: balloon page
+4 $d015 c! \ enable sprite 2
+13 $7fa c! \ sprite 2 data from 13th block
+sprite $340 63 move \ copy sprite to 13th block
+begin 200 0 do 
+vsync vsync vsync vsync \ slow down
+i $d004 c! i $d005 c! \ move sprite
+loop again ;
+
+compile balloon
--- run_examples.sh
+++ run_examples.sh
@@ -1,4 +1,9 @@
 set -e
+
+./acmeforth examples/balloon.fs
+acme -o balloon.prg -f cbm balloon.asm
+x64 balloon.prg
+
 ./acmeforth examples/sierp.fs
 acme -o sierp.prg -f cbm sierp.asm
 x64 sierp.prg
